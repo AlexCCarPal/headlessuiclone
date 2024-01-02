52891036322f196215fe925b5433ad9d30f56d47
AlexC <pluspatilla@gmail.com>
2024-01-02 02:13:14 -0500

    remove vue and tailwindcss

--- packages/@headlessuiclone-tailwindcss/build/index.cjs
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-let plugin =
-  process.env.NODE_ENV === 'production'
-    ? require('./headlessuiclone.prod.cjs')
-    : require('./headlessuiclone.dev.cjs')
-
-module.exports = (plugin.__esModule ? plugin : { default: plugin }).default
